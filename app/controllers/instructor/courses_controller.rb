06636001d679e3a4a004297550e1c24a596c8f74
Rebecca <rebecca.sinkoff@gmail.com>
2017-10-20 20:06:33 +0000

    Updated authorizations to all instructor pages

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -15,10 +15,21 @@ class Instructor::CoursesController < ApplicationController
     end
     
     def show
-       @course = Course.find(params[:id]) 
+        
     end
     
     private
+    
+    def require_authorized_for_current_course
+        if current_course.user != course_user
+            render plain: 'Unauthorized', status: :unauthorized
+        end
+    end
+    
+    helper_method :current_course
+    def current_course
+       @current_course ||= Course.find(params[:id]) 
+    end
     
     def course_params
        params.require(:course).permit(:title, :description, :cost) 
